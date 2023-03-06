db7210945ce50ef8e4c38287e2ce8308529d6c9a
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-06 16:17:28 +0100

    added functions added red cell created the situation of loss

--- js/script.js
+++ js/script.js
@@ -15,55 +15,103 @@ const refreshBtnEl = document.getElementById("refresh-btn");
 
 const refreshEl = document.getElementById("refresh");
 
+const youLose = document.getElementById("you-lose");
+const youWin = document.getElementById("you-win");
+
 // Creo una variabile per bersagliare il container dove creeremo la griglia
 const gridEl = document.getElementById("grid");
 
 // Creo variabile per bersagliare il selettore per la difficoltà
 const difficultyEl = document.getElementById("difficulty-select");
 
+// Variabile per terminare non rendere piu cliccabile la griglia
+let endGame = false;
+
+
+
 
 // Creo un evento al click del button
 playBtnEl.addEventListener("click", function() {
-
+    
     // al click il play button sparisce
     playBtnEl.style.display = "none";
     // e compare il button per ricaricare la pagina e giocare di nuovo
     refreshEl.style.display = "block";
-
+    
     // al click il display della griglia passa da none a flex
     gridEl.style.display = "flex";
-
+    
     // Creo variabile per usare il valore della difficoltà selezionato
     let difficulty = difficultyEl.value;
+    
+    // Creo un array di 16 numeri casuali
+    const bombs = [];
+
+    const clicked = [];
+    
+    
+
+    console.log(bombs);
+    console.log(clicked);
 
     if (difficulty == 1) {
 
         // Creo un ciclo che genera le celle all'interno della griglia
         for (let i = 1; i <= 100; i++) {
             
-            // Creo variabile per creare nuovo elemento in pagina
-            let squareEl = document.createElement("div");
-            // gli asseglo una classe
-            squareEl.className = "square";
-            // faccio in modo che ci siano 10 celle per ogni riga
-            squareEl.style.width = "calc(100% / 10)";
-            // faccio comparire un numero crescente da 1 a 100 al suo interno usando i
-            squareEl.innerHTML = i;
-            // dico che l'elemento deve essere aggiunto all'interno della griglia
-            gridEl.append(squareEl);
+            const squareEl = createSquare(i, 10, gridEl);
+
+            createRandomNum(bombs, 100);
         
+            refreshBtnEl.addEventListener("click", function(){
+                window.location.reload();
+            });
+
             // creo un evento al click della cella
             squareEl.addEventListener("click", function(){
 
-            // al click della cella deve essere alternativamente aggiunta e tolta la classe active
-            squareEl.classList.toggle("active");
-            
-            // Mostro in console il numero della cella cliccata
-            console.log(`Hai cliccato la cella n. ${i}`);
-            });
-    
-            refreshBtnEl.addEventListener("click", function(){
-            window.location.reload();
+                if(endGame) {
+                    return;
+                };
+
+                console.log(bombs);
+
+                if (bombs.includes(i)){
+
+                    gridEl.style.filter = "blur(2px)";
+
+                    youLose.style.display = "block";
+
+                    squareEl.style.backgroundColor = "red";
+                    squareEl.style.color = "white";
+
+
+                    endGame = true;
+
+
+                } else {
+
+                    // al click della cella deve essere aggiunta la classe active
+                    squareEl.classList.add("active");
+
+                    // clicked.push(parseInt(i));
+                    
+
+                    // if (clicked.lenght == 100 - 16){
+
+                    //     gridEl.style.filter = "blur(2px)";
+
+                    //     youWin.style.display = "block";
+
+                    //     endGame = true;
+                    // };
+                    
+                    // Mostro in console il numero della cella cliccata
+                    console.log(`Hai cliccato la cella n. ${i}`);
+
+
+                };
+                
             });
         };
         
@@ -71,63 +119,122 @@ playBtnEl.addEventListener("click", function() {
 
         // Creo un ciclo che genera le celle all'interno della griglia
         for (let i = 1; i <= 81; i++) {
-            // Creo variabile per creare nuovo elemento in pagina
-            let squareEl = document.createElement("div");
-            // gli asseglo una classe
-            squareEl.className = "square";
-            // faccio in modo che ci siano 10 celle per ogni riga
-            squareEl.style.width = "calc(100% / 9)";
-            // faccio comparire un numero crescente da 1 a 100 al suo interno usando i
-            squareEl.innerHTML = i;
-            // dico che l'elemento deve essere aggiunto all'interno della griglia
-            gridEl.append(squareEl);
-        
+
+            const squareEl = createSquare(i, 9, gridEl);
+
+            createRandomNum(bombs, 81);
+
+            refreshBtnEl.addEventListener("click", function(){
+            window.location.reload();
+            });
+
             // creo un evento al click della cella
-           squareEl.addEventListener("click", function(){
+            squareEl.addEventListener("click", function(){
 
-           // al click della cella deve essere alternativamente aggiunta e tolta la classe active
-           squareEl.classList.toggle("active");
-        
-           // Mostro in console il numero della cella cliccata
-           console.log(`Hai cliccato la cella n. ${i}`);
-           } );
+                if(endGame) {
+                    return;
+                };
 
-           refreshBtnEl.addEventListener("click", function(){
-           window.location.reload();
-           });
+                if (bombs.includes(i)){
+
+                    squareEl.style.backgroundColor = "red";
+                    squareEl.style.color = "white";
+
+                    endGame = true;
+
+                } else {
+
+                    // al click della cella deve essere aggiunta la classe active
+                    squareEl.classList.add("active");
+                    
+                    // Mostro in console il numero della cella cliccata
+                    console.log(`Hai cliccato la cella n. ${i}`);
+                };
+                
+            });
+
         };
            
     } else if (difficulty == 3) {
 
         // Creo un ciclo che genera le celle all'interno della griglia
         for (let i = 1; i <= 49; i++) {
-            // Creo variabile per creare nuovo elemento in pagina
-            let squareEl = document.createElement("div");
-            // gli asseglo una classe
-            squareEl.className = "square";
-            // faccio in modo che ci siano 10 celle per ogni riga
-            squareEl.style.width = "calc(100% / 7)";
-            // faccio comparire un numero crescente da 1 a 100 al suo interno usando i
-            squareEl.innerHTML = i;
-            // dico che l'elemento deve essere aggiunto all'interno della griglia
-            gridEl.append(squareEl);
-       
+
+            const squareEl = createSquare(i, 7, gridEl);
+
+            createRandomNum(bombs, 49);
+
+            refreshBtnEl.addEventListener("click", function(){
+            window.location.reload();
+            });
+
             // creo un evento al click della cella
-           squareEl.addEventListener("click", function(){
-    
-           // al click della cella deve essere alternativamente aggiunta e tolta la classe active
-           squareEl.classList.toggle("active");
-        
-           // Mostro in console il numero della cella cliccata
-           console.log(`Hai cliccato la cella n. ${i}`);
-           });
-    
-           refreshBtnEl.addEventListener("click", function(){
-           window.location.reload();
-           });
+            squareEl.addEventListener("click", function(){
+
+                if(endGame) {
+                    return;
+                };
+
+                if (bombs.includes(i)){
+
+                    squareEl.style.backgroundColor = "red";
+                    squareEl.style.color = "white";
+
+                    endGame = true;
+
+                } else {
+
+                    // al click della cella deve essere aggiunta la classe active
+                    squareEl.classList.add("active");
+                    
+                    // Mostro in console il numero della cella cliccata
+                    console.log(`Hai cliccato la cella n. ${i}`); 
+                };
+                
+            });
         };
     };   
 });
-        
+
+
+// Functions
+
+// Funzione per creare le celle, il numero all'interno e definire la lunghezza delle righe
+
+function createSquare (cellNum, cellRow, container) {
+
+    // Creo variabile per creare nuovo elemento in pagina
+    let squareEl = document.createElement("div");
+    // gli asseglo una classe
+    squareEl.className = "square";
+    // faccio in modo che ci siano 10 celle per ogni riga
+    squareEl.style.width = `calc(100% / ${cellRow}`;
+    // faccio comparire un numero crescente da 1 a 100 al suo interno usando i
+    squareEl.innerHTML = cellNum;
+    // dico che l'elemento deve essere aggiunto all'interno della griglia
+    container.append(squareEl);
+
+    return squareEl;
+
+};
+
+// Funzione per creare 16 numeri random diversi tra loro e inserirli in un array
+
+function createRandomNum (array, cellNum) {
+
+    while (array.length < 16){
+   
+        let newNumber = Math.floor(Math.random() * cellNum) + 1;
+    
+        if (!array.includes(newNumber)) {
+    
+            array.push(newNumber);
+        };
+    };
+};
+
+// Funzione per terminare il gioco
+
+
 
     
